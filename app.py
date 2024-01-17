a03bc23643a1ed2161102a0610a34a70a820888f
Theophile Tagne <tagnepierre9@gmail.com>
2024-01-17 23:10:56 +0100

    integrate Flask into the project

---
+++ app.py
@@ -0,0 +1,27 @@
+import json
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.get("/data.json")
+def get_data_json():
+    try:
+        # TODO: Utilise un chemin absolu en prod
+        # exemple: data_file_path = '/path/to/your/data.json'
+        with open("data.json", encoding="utf-8") as f:
+            data = json.load(f)
+        return jsonify(data)
+    except FileNotFoundError:
+        return jsonify({"error": "Data file not found"}), 404
+    except json.JSONDecodeError:
+        return jsonify({"error": "Error decoding JSON data"}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  # TODO: Mettre debug=False en production
