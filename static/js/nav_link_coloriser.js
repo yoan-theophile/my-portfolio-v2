5ea39e58d752a16cd6d3b5c7c1f8898e564c5ce4
Theophile Tagne <86710194+yoan-theophile@users.noreply.github.com>
2024-01-19 13:38:47 +0100

    Style vert cassé  (#2)
    * style: fix overflow 'realisation'
    update primary to light green
    
    * style: update navbar background color depending on the current section

--- static/js/nav_link_coloriser.js
+++ static/js/nav_link_coloriser.js
@@ -19,4 +19,32 @@ const addOrRemoveActiveClass = () => {
     })
 }
 
+/**
+ * Update the background color of the navbar depending on the position the current section
+ */
+const updateNavbarBackgroundColor = () => {
+    const sectionColors = {
+        home: 'rgba(121, 191, 137, 0.3)', // #79bf89 avec une transparence de 70%
+        about: 'rgba(133, 133, 200, 0.3)', // #79bf89 avec une transparence de 70%
+        achievement: 'rgba(237, 229, 226, 0.3)', // #ede5e2 avec une transparence de 70%
+        contact: 'rgba(245, 245, 245, 0.3)', // #f5f5f5 avec une transparence de 70%
+    }
+
+    let sectionLinks = document.querySelectorAll('#home,#about,#achievement,#contact')
+    const navbar = document.querySelector('.container.navbar');
+
+    window.addEventListener('scroll', event => {
+        scrollPos = window.scrollY
+
+        sectionLinks.forEach((section) => {
+             if (scrollPos + 100 > section.offsetTop && scrollPos + 100 < section.offsetTop + section.offsetHeight) {
+                navbar.style.backgroundColor = sectionColors[section.id]
+             } else {
+                
+             }
+        })
+    })
+}
+
 addOrRemoveActiveClass()
+updateNavbarBackgroundColor()
